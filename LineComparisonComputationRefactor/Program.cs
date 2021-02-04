2af140791649712da8516ab214737ffd545f9077
himanshunete <himanshuneteh@gmail.com>
2021-02-05 00:08:48 +0530

    [himanshunete]Add CompareTwoLines

--- LineComparisonComputationRefactor/Program.cs
+++ LineComparisonComputationRefactor/Program.cs
@@ -2,28 +2,43 @@
 
 namespace LineComparisonComputationRefactor
 {
+
+    class Points
+    {
+        double x1x;
+        double y1x;
+        double x2x;
+        double y2x;
+        double x1y;
+        double y1y;
+        double x2y;
+        double y2y;
+        public Points(double x1x, double y1x, double x2x, double y2x, double x1y, double y1y, double x2y, double y2y)
+        {
+            this.x1x = 0;
+            this.y1x = 0;
+            this.x2x = 0;
+            this.y2x = 0;
+            this.x1y = 0;
+            this.y1y = 0;
+            this.x2y = 0;
+            this.y2y = 0;
+
+        }
+    }
     class Program
     {
-        public static void LineComparison()
-        {
-            Console.WriteLine(" Welcome To Line Comparison Computation Program ");
-            //Coordinates of Line1
-            double x1x = Convert.ToDouble(Console.ReadLine());
-            double y1x = Convert.ToDouble(Console.ReadLine());
-            double x2x = Convert.ToDouble(Console.ReadLine());
-            double y2x = Convert.ToDouble(Console.ReadLine());
+      
 
-            //Coordinates of Line2
-            double x1y = Convert.ToDouble(Console.ReadLine());
-            double y1y = Convert.ToDouble(Console.ReadLine());
-            double x2y = Convert.ToDouble(Console.ReadLine());
-            double y2y = Convert.ToDouble(Console.ReadLine());
-            double squareXx = (x2x - x1x) * (x2x - x1x);
-            double squareYx = (y2x - y1x) * (y2x - y1x);
-            double squareXy = (x2y - x1y) * (x2y - x1y);
-            double squareYy = (y2y - y1y) * (y2y - y1y);
-            double lengthOfLine1 = Math.Sqrt(squareXx + squareYx);
-            double lengthOfLine2 = Math.Sqrt(squareXy + squareYy);
+        public static void LineComparison(double x1x, double y1x, double x2x, double y2x, double x1y, double y1y, double x2y, double y2y)
+        {
+
+            double squareX1 = (x2x - x1x) * (x2x - x1x);
+            double squareY1 = (y2x - y1x) * (y2x - y1x);
+            double squareX2 = (x2y - x1y) * (x2y - x1y);
+            double squareY2 = (y2y - y1y) * (y2y - y1y);
+            double lengthOfLine1 = Math.Sqrt(squareX1 + squareY1);
+            double lengthOfLine2 = Math.Sqrt(squareX2 + squareY2);
 
             if (lengthOfLine1 == lengthOfLine2)
             {
@@ -42,7 +57,19 @@ namespace LineComparisonComputationRefactor
 
         static void Main(string[] args)
         {
-            LineComparison();
+            Console.WriteLine(" Welcome To Line Comparison Computation Program ");
+            double x1x = Convert.ToDouble(Console.ReadLine());
+            double y1x = Convert.ToDouble(Console.ReadLine());
+            double x2x = Convert.ToDouble(Console.ReadLine());
+            double y2x = Convert.ToDouble(Console.ReadLine());
+            double x1y = Convert.ToDouble(Console.ReadLine());
+            double y1y = Convert.ToDouble(Console.ReadLine());
+            double x2y = Convert.ToDouble(Console.ReadLine());
+            double y2y = Convert.ToDouble(Console.ReadLine());
+
+
+            LineComparison(x1x, y1x, x2x, y2x, x1y, y1y, x2y, y2y);
+
         }
     }
 }
