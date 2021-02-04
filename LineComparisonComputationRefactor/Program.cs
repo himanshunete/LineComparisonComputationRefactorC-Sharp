ca1e598f76db8360d78015612056c8cecbf8b1ae
himanshunete <himanshuneteh@gmail.com>
2021-02-04 21:53:57 +0530

    [himanshunete]Add FirstCommit

---
+++ LineComparisonComputationRefactor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LineComparisonComputationRefactor
+{
+    class Program
+    {
+        public static void LineComparison()
+        {
+            Console.WriteLine(" Welcome To Line Comparison Computation Program ");
+        }
+
+        static void Main(string[] args)
+        {
+
+            LineComparison();
+        }
+    }
+}
