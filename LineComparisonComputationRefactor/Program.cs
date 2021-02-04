3899a4e5918e962555f771e59ed8f7d0c2d2f35b
himanshunete <himanshuneteh@gmail.com>
2021-02-04 22:17:37 +0530

    [himanshunete]Add FindLengthOfALine

--- LineComparisonComputationRefactor/Program.cs
+++ LineComparisonComputationRefactor/Program.cs
@@ -7,11 +7,19 @@ namespace LineComparisonComputationRefactor
         public static void LineComparison()
         {
             Console.WriteLine(" Welcome To Line Comparison Computation Program ");
+            //Coordinates
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double squareX = (x2 - x1) * (x2 - x1);
+            double squareY = (y2 - y1) * (y2 - y1);
+            Console.WriteLine($" Length of a Line: { Math.Sqrt(squareX + squareY) }");
         }
 
         static void Main(string[] args)
         {
-
             LineComparison();
         }
     }
